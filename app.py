dabd5bcdfedd82877c69c21cb85acb3077989fd5
Jaechan Lee <chanylee77@gmail.com>
2022-02-07 13:17:18 -0800

    hello

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__, static_url_path='', static_folder='./frontend/build')
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
 api = Api(app)
 
@@ -12,4 +12,4 @@ api = Api(app)
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
-api.add_resource(HelloApiHandler, '/flask/hello')
+api.add_resource(HelloApiHandler, '/flask/hello') 
